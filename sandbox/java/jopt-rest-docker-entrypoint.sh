ed948017e5b13c462d032904a7f20201c482d562
jensrichter <jensrichter24@googlemail.com>
2022-04-13 17:47:04 +0200

    fix entry point

--- sandbox/java/jopt-rest-docker-entrypoint.sh
+++ sandbox/java/jopt-rest-docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 echo "Trying to download Jopt Rest Client Examples"
 
 folder="/home/coder/project/jopt.examples/"
-url="https://github.com/DNA-Evolutions/DNA-Rest-Java-Rest-Client-Examples.git"
+url="https://github.com/DNA-Evolutions/Java-REST-Client-Examples.git"
 if ! git clone "${url}" "${folder}" 2>/dev/null && [ -d "${folder}" ] ; then
     echo "Clone skipped as the folder ${folder} already exists. If you want a 'fresh' clone, choose another volume or rename the existing folder."
 fi
