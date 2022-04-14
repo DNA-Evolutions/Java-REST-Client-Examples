6d2630e1b8e8958621fa430423a626eec43c0c61
jensrichter <jensrichter24@googlemail.com>
2022-04-14 10:00:22 +0200

    move default sandbox directory

--- sandbox/java/jopt-rest-docker-entrypoint.sh
+++ sandbox/java/jopt-rest-docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -eu
 
-echo "Trying to download Jopt Rest Client Examples"
+echo "Trying to download JOpt Rest Client Examples"
 
-folder="/home/coder/project/jopt.examples/"
+folder="/home/coder/project/jopt.rest.examples/"
 url="https://github.com/DNA-Evolutions/Java-REST-Client-Examples.git"
 if ! git clone "${url}" "${folder}" 2>/dev/null && [ -d "${folder}" ] ; then
     echo "Clone skipped as the folder ${folder} already exists. If you want a 'fresh' clone, choose another volume or rename the existing folder."
